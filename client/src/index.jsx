caedbf19b4569ccd3872a9f663c25658a764cee4
Eric Meltzer <meltzer.eric@live.com>
2018-07-13 18:38:15 -0400

    Add file upload

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,31 +2,22 @@ import React from 'react';
 import ReactDom from "react-dom";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {};
   }
 
-  render(){
-    return (<div><div className="row">
-      <span className="col-xs-6">
-      Some text!
-      </span>
-      <span className="col-xs-6">
-      Some more text!
-      </span>
-      </div>
-      <div className="row">
-        <span className="col-xs-6">
-        Some text!
-        </span>
-        <span className="col-xs-6">
-        Some more text!
-        </span>
+  render() {
+    return (<div>
+      <form role="form" class="form" onsubmit="return false;">
+        <div class="form-group">
+          <label for="file">File</label>
+          <input id="file" type="file" class="form-control"/>
         </div>
-        </div>
-    )
+        <button id="upload" type="button" class="btn btn-primary">Upload</button>
+      </form>
+    </div>)
   }
 }
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(<App/>, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -5,21 +5,7 @@ var app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser());
 
-app.post('/repos', function (req, res) {
-  util.getReposByUsername(req.body.term, res);
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
-});
-
-app.get('/repos', function (req, res) {
-  util.getReposFromMongo(req.query.term, res);
-  // TODO - your code here!
-  // This route should send back the top 25 repos
-});
-
-var port = process.env.PORT || 1128;
+var port = process.env.PORT || 1320;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
