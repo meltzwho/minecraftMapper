2c373e1660aa71db5de665fdbde7a881786a47b2
Eric Meltzer <meltzer.eric@live.com>
2018-07-13 16:44:24 -0400

    Testing heroku build tools

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const util = require('../helpers/github.js');
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
