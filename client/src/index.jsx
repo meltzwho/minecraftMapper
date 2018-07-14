2b0fa53c263e341a40b19f25eaa515d639bc1e37
Eric Meltzer <meltzer.eric@live.com>
2018-07-14 13:49:32 -0400

    MVP

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,21 +5,56 @@ import axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      worlds: []
+    };
   }
 
   handleUpload() {
     var file = new FormData();
     file.append('foo', 'bar');
     file.append('file', document.getElementById('file').files[0]);
-    axios.post('/upload', file);
+    axios.post('/upload', file)
+         .then((res) => {
+           let oldWorlds = this.state.worlds.slice();
+           if(this.state.worlds.indexOf(res.dat) === -1) oldWorlds.push(res.data);
+           this.setState({
+             worlds: oldWorlds
+           });
+           console.log(this.state.worlds);
+         })
+         .catch((err)=>{
+           console.error(err);
+         });
+  }
+
+  componentDidMount(){
+    axios.get('/worlds')
+    .then((res)=>{
+      res.data.forEach((world) => {
+        world = world.split('/');
+        world = world[world.length-1];
+        this.state.worlds.push(world);
+      })
+      this.setState({});
+    })
+    .catch((err)=>{
+      console.error(err);
+    })
   }
 
   render() {
+    let worlds = [];
+    for(let world of this.state.worlds){
+      worlds.push(<p><a href={world+'/index.html'} target="_blank">{world.split('-')[0]}</a></p>);
+    }
     return (<div>
       <label>File</label>
       <input id="file" type="file" className="form-control"/>
       <button id="upload" type="button" className="btn btn-primary" onClick={() => this.handleUpload()}>Upload</button>
+      <ul>
+      {worlds}
+      </ul>
     </div>)
   }
 }
--- server/index.js
+++ server/index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const path = require('path');
+const exec = require('child_process').exec;
 const formidable = require('formidable');
 const fs = require('fs');
+const unzip = require('unzip');
+const { lstatSync, readdirSync } = require('fs');
+const { join } = require('path');
+
+const isDirectory = source => lstatSync(source).isDirectory();
+const getDirectories = source =>
+  readdirSync(source).map(name => join(source, name)).filter(isDirectory);
+
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
@@ -11,15 +20,29 @@ app.use(bodyParser());
 
 var port = process.env.PORT || 1320;
 
+app.get('/worlds', (req, res)=>{
+  res.send(JSON.stringify(getDirectories(__dirname + '/../client/dist')));
+});
+
 app.post('/upload', (req, res) => {
   var form = new formidable.IncomingForm();
   form.parse(req, function(err, fields, files) {
+    if (files.file === undefined) {
+      return;
+    }
     var oldpath = files.file.path;
     var newpath = path.join(__dirname, files.file.name);
     fs.rename(oldpath, newpath, function(err) {
       if (err)
-        throw err;
-      res.end();
+        return console.error(err);
+      fs.createReadStream(path.join(__dirname, files.file.name))
+      .pipe(unzip.Extract({path: __dirname}))
+      .on('close', () => {
+        let randomDir = files.file.name.slice(0, -4) + "-" + Math.random() * Number.MAX_SAFE_INTEGER;
+        exec(`overviewer.py ${path.join(__dirname, files.file.name.slice(0, -4))} ${path.join(__dirname + `/../client/dist/${randomDir}`)}`, (err, stdo, stde) => {
+          res.send(randomDir);
+        });
+      });
     });
   });
   //external command curl --upload-file transfer.sh <myfile>
