603816d9114ca8d5c7c16113e1996b3f2bfd3769
Eric Meltzer <meltzer.eric@live.com>
2018-07-13 21:17:12 -0400

    Testing heroku

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDom from "react-dom";
+import ReactDom from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,15 +8,18 @@ class App extends React.Component {
     this.state = {};
   }
 
+  handleUpload() {
+    var file = new FormData();
+    file.append('foo', 'bar');
+    file.append('file', document.getElementById('file').files[0]);
+    axios.post('/upload', file);
+  }
+
   render() {
     return (<div>
-      <form role="form" class="form" onsubmit="return false;">
-        <div class="form-group">
-          <label for="file">File</label>
-          <input id="file" type="file" class="form-control"/>
-        </div>
-        <button id="upload" type="button" class="btn btn-primary">Upload</button>
-      </form>
+      <label>File</label>
+      <input id="file" type="file" className="form-control"/>
+      <button id="upload" type="button" className="btn btn-primary" onClick={() => this.handleUpload()}>Upload</button>
     </div>)
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const path = require('path');
+const formidable = require('formidable');
+const fs = require('fs');
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser());
 
 var port = process.env.PORT || 1320;
+
+app.post('/upload', (req, res) => {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    var oldpath = files.file.path;
+    var newpath = path.join(__dirname, files.file.name);
+    fs.rename(oldpath, newpath, function(err) {
+      if (err)
+        throw err;
+      res.end();
+    });
+  });
+  //external command curl --upload-file transfer.sh <myfile>
+});
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
